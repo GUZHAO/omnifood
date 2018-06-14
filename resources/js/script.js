5186f2583f74bc9b36ff8c4709323c6c75a22ae0
GUZHAO <guzhao1990@gmail.com>
2018-06-13 22:27:08 -0400

    updated jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,6 @@
-// $(document).ready(function () {
-//     $('h1').click(function () {
-//         $(this).css('background-color', '#ff0000');
-//     })
-// })
-
+/*Sticky Navigation*/
 $(function () {
-    $('.js--section--features').waypoint(function (direction) {
+    $('.js--section-features').waypoint(function (direction) {
         if (direction === "down") {
             $('nav').addClass('sticky');
         } else {
@@ -14,4 +9,56 @@ $(function () {
     }, {
         offset: '60px;'
     });
+});
+
+/*Scroll on button*/
+$('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+});
+
+$('.js--scroll-to-start').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
+});
+
+/*Animation on scroll*/
+$('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    },
+    {
+        offset: '50%'
+    });
+
+$('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },
+    {
+        offset: '50%'
+    });
+
+$('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    },
+    {
+        offset: '50%'
+    });
+
+$('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    },
+    {
+        offset: '50%'
+    });
+
+$('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+    } else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+    }
 });
