e314456fd1c0a5613a8cef4fdb7c2adf0d277ffc
GUZHAO <guzhao1990@gmail.com>
2018-08-04 13:54:37 -0400

    added map

--- resources/js/script.js
+++ resources/js/script.js
@@ -62,3 +62,24 @@ $('.js--nav-icon').click(function () {
         icon.removeClass('ion-close-round');
     }
 });
+
+/* Maps */
+var map = new GMaps(
+    {
+        div: '.map',
+        lat: 38.7436266,
+        lng: -9.1602037,
+        zoom: 12
+    }
+);
+
+map.addMarker(
+    {
+        lat: 38.7436266,
+        lng: -9.1602037,
+        title: Lisbon,
+        infoWindow: {
+            content: '<p>Our Lisbon HQ</p>'
+        }
+    }
+);
